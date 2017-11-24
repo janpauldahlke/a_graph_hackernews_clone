332299ce5032e34f82b3c8ad2f4805f718679532
janpauldahlke <janpauldahlke@gmail.com>
2017-11-24 14:21:29 +0100

    wrap app in apolloprovider

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//add apollo related stuff
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink} from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+//point to API, the sticky end!
+const httpLink = new HttpLink({uri: 'http://localhost:60000/simple/v1/cjadthxsv0004011107e6fpnm'});
+
+//prepare client
+const client = new ApolloClient({
+  link: httpLink,
+  cache : new InMemoryCache()
+});
+
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
