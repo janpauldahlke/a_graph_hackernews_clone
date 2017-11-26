b45378195ed9bfeca435fb874d6817b4f72a92d3
janpauldahlke <janpauldahlke@gmail.com>
2017-11-26 13:40:33 +0100

    some research about non native DOM elements and onClick

--- src/components/IconLike.js
+++ src/components/IconLike.js
@@ -4,7 +4,9 @@ class IconLike extends Component {
 
   render(){
     return(
-      <div className="float-right" style={{width: '32px', height: '32px', display: 'inline-block'}}>
+      <div
+        onClick={this.props.onClick}
+        className="float-right" style={{width: '32px', height: '32px', display: 'inline-block'}}>
       <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"x="0px" y="0px"
       	 viewBox="0 0 484.4 484.4">
       <g>
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
 import IconLike from './IconLike.js'
+import timeDifferenceForDate from '../util/timeDifference.js';
 
 
 class Link extends Component {
-
-  createLinkElement(){
-
-  }
-
 
   render () {
 
@@ -16,7 +12,13 @@ class Link extends Component {
       <li className="list-group-item">
         <span>{this.props.link.description}</span>&nbsp;
         <a href={this.props.link.url}>{this.props.link.url}</a>&nbsp;
-          <IconLike />
+          <IconLike
+
+            onClick={() => {
+              console.log('onClick');
+              /*https://stackoverflow.com/questions/28268835/react-onclick-event-on-component*/
+            }}
+            />
       </li>
     );
   }
